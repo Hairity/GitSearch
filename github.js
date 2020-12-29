f1f6db7e17aa0416c933c961fac2f100dc8c14da
Rauno Uustalu <raunoize@gmail.com>
2020-12-29 12:57:41 +0200

    first realse

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'f9f47bd68935f1133faa';
+    this.client_secret = '276c604455e031733203b9b3b5ca613a3630b649'
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
